18435d0246ada337859c1a7c2573b55f5d979de7
huntercodecamp <huntercodecamp@gmail.com>
2021-10-05 14:43:04 -0300

    new file:_front/devmap.php, modified:_front/menu.php, modified:index.php

--- _front/menu.php
+++ _front/menu.php
@@ -4,36 +4,38 @@
 	<input type="checkbox" id="bt-equiv" checked >
 	<label for="bt-equiv" id="lbequiv"> &equiv; </label>
 	<div class="dMenu">
-		<a href="painel.php"> Início </a>
+		<a href="painel.php"> Home </a>
 		<input id="bt-cadastro" type="checkbox" name="subs" class="SInput-1"  >
-		<label for="bt-cadastro" class="LbSubMenu-1">Cadastros</label>
+		<label for="bt-cadastro" class="LbSubMenu-1">
+			Registers
+		</label>
 		<div id="Sub-1 " >
-			<a href="#"> Cadastrar Funcionário </a>
-			<a href="#"> Cadastrar Salas </a>
-			<a href="#"> Cadastrar Equipamentos </a>
+			<a href="#"> Register Collaborator </a>
+			<a href="#"> Register Room </a>
+			<a href="#"> Register Equipment </a>
 			
 			<a href="CadastrarUsuario.php"> Cadastrar Cliente </a>
-			
+			<a href="CadastrarTurmas.php">  </a>
 			<a href="#"> Cadastrar Serviços </a>
 			<a href="#"> Cadastrar Produtos </a>
 			<a href="#"> Cadastrar Ordem de Serviços </a>
 		</div>
 		<input id="visualizar" type="checkbox" name="subs" class="SInput-2" >
-		<label for="visualizar" class="LbSubMenu-2"> Visualizar </label>
+		<label for="visualizar" class="LbSubMenu-2"> View </label>
 		<div id="Sub-2"  >
-			<a href="#"> Visualizar Client </a>
-			<a href="#"> Visualizar Funcionários </a>
-			<a href="#"> Visualizar Serviço </a>
-			<a href="#"> Visualizar Produtos </a>
-			<a href="#"> Visualizar Ordem de Serviço </a>
+			<a href="#"> View Client </a>
+			<a href="#"> View employees </a>
+			<a href="#"> View Work </a>
+			<a href="#"> View Product </a>
+			<a href="#"> View Work Order </a>
 		</div>
-		<a href="#"> Mapa de Desenvolvimento</a> 
+		<a href="#"> Development Progress </a> 
 		<a href="#"> Sair </a> 
 	</div>
 	
 	<figure>
 		<img src="_imgs/logo.jpg" alt="logo" />
 	</figure>
-	<h2> Sistema de O.S </h2>
+	<h2> School System</h2>
 	
 </nav>
--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title> Untitle School </title>
+	<title> School System </title>
 	<link rel="stylesheet" href="_css/main-style.css">
 	<link rel="stylesheet" href="_css/menu-style.css">
 </head>
 <body>
 	<?php 
 	require_once('_front/menu.php');
-
+	require_once('')
 
 	?>
 </body>
