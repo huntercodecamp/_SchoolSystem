c8fb0ddce22bc878c54f54b60da1113d74c377a6
huntercodecamp <huntercodecamp@gmail.com>
2021-10-05 14:12:08 -0300

    menu

---
+++ _front/menu.php
@@ -0,0 +1,39 @@
+ 
+<nav>
+
+	<input type="checkbox" id="bt-equiv" checked >
+	<label for="bt-equiv" id="lbequiv"> &equiv; </label>
+	<div class="dMenu">
+		<a href="painel.php"> Início </a>
+		<input id="bt-cadastro" type="checkbox" name="subs" class="SInput-1"  >
+		<label for="bt-cadastro" class="LbSubMenu-1">Cadastros</label>
+		<div id="Sub-1 " >
+			<a href="#"> Cadastrar Funcionário </a>
+			<a href="#"> Cadastrar Salas </a>
+			<a href="#"> Cadastrar Equipamentos </a>
+			
+			<a href="CadastrarUsuario.php"> Cadastrar Cliente </a>
+			
+			<a href="#"> Cadastrar Serviços </a>
+			<a href="#"> Cadastrar Produtos </a>
+			<a href="#"> Cadastrar Ordem de Serviços </a>
+		</div>
+		<input id="visualizar" type="checkbox" name="subs" class="SInput-2" >
+		<label for="visualizar" class="LbSubMenu-2"> Visualizar </label>
+		<div id="Sub-2"  >
+			<a href="#"> Visualizar Client </a>
+			<a href="#"> Visualizar Funcionários </a>
+			<a href="#"> Visualizar Serviço </a>
+			<a href="#"> Visualizar Produtos </a>
+			<a href="#"> Visualizar Ordem de Serviço </a>
+		</div>
+		<a href="#"> Mapa de Desenvolvimento</a> 
+		<a href="#"> Sair </a> 
+	</div>
+	
+	<figure>
+		<img src="_imgs/logo.jpg" alt="logo" />
+	</figure>
+	<h2> Sistema de O.S </h2>
+	
+</nav>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Untitle School </title>
+	<link rel="stylesheet" href="_css/main-style.css">
+	<link rel="stylesheet" href="_css/menu-style.css">
+</head>
+<body>
+	<?php 
+	require_once('_front/menu.php');
+
+
+	?>
+</body>
+</html>
