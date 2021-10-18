5e7edc652d4ecd4358fdf3ae635ffe755738037d
huntercodecamp <huntercodecamp@gmail.com>
2021-10-18 14:44:03 -0300

    concertando

--- _front/footer.php
+++ _front/footer.php
@@ -1,36 +1,36 @@
 <footer>
 	<div class="column">
 		<ul>
-			<li><a href="#">item1</a></li>
-			<li><a href="#">item2</a></li>
-			<li><a href="#">item3</a></li>
-			<li><a href="#">item4</a></li>
-			<li><a href="#">item5</a></li>
-			<li><a href="#">item6</a></li>
-			<li><a href="#">item7</a></li>
-			<li><a href="#">item8</a></li>
+			<li><a href="#">item01</a></li>
+			<li><a href="#">item02</a></li>
+			<li><a href="#">item03</a></li>
+			<li><a href="#">item04</a></li>
+			<li><a href="#">item05</a></li>
+			<li><a href="#">item06</a></li>
+			<li><a href="#">item07</a></li>
+			<li><a href="#">item08</a></li>
 		</ul>
 		
 		<ul>
-			<li><a href="#">item1</a></li>
-			<li><a href="#">item2</a></li>
-			<li><a href="#">item3</a></li>
-			<li><a href="#">item4</a></li>
-			<li><a href="#">item5</a></li>
-			<li><a href="#">item6</a></li>
-			<li><a href="#">item7</a></li>
-			<li><a href="#">item8</a></li>
+			<li><a href="#">item09</a></li>
+			<li><a href="#">item10</a></li>
+			<li><a href="#">item11</a></li>
+			<li><a href="#">item12</a></li>
+			<li><a href="#">item13</a></li>
+			<li><a href="#">item14</a></li>
+			<li><a href="#">item15</a></li>
+			<li><a href="#">item16</a></li>
 		</ul>
 		
 		<ul>
-			<li><a href="#">item1</a></li>
-			<li><a href="#">item2</a></li>
-			<li><a href="#">item3</a></li>
-			<li><a href="#">item4</a></li>
-			<li><a href="#">item5</a></li>
-			<li><a href="#">item6</a></li>
-			<li><a href="#">item7</a></li>
-			<li><a href="#">item8</a></li>
+			<li><a href="#">item17</a></li>
+			<li><a href="#">item18</a></li>
+			<li><a href="#">item19</a></li>
+			<li><a href="#">item20</a></li>
+			<li><a href="#">item21</a></li>
+			<li><a href="#">item22</a></li>
+			<li><a href="#">item23</a></li>
+			<li><a href="#">item24</a></li>
 		</ul>
 	</div>
 </footer>
