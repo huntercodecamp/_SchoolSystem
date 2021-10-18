12d5f8104ed892a9231c68ed49d5298acf75eb5d
Fernando Silveira <88800990+huntercodecamp@users.noreply.github.com>
2021-10-18 15:24:00 -0300

    Update RegisterCollaborator.php

--- _front/RegisterCollaborator.php
+++ _front/RegisterCollaborator.php
@@ -13,33 +13,74 @@ require_once('top.php');
 </head>
 
 <body>
-	<div id="main">
-		<?php require_once("menu.php"); ?>
-		<form action="CadastroOS_submit" method="post" accept-charset="utf-8">
-			<article >
-				<label for=""> Nº </label>
-				<div id="N_OS">
-
-				</div>
-
-				<label for="">
-
-				</label>
+	<article id="cadastrouser">
+	
+	<form action="">
+		<h1>
+			Cadastrar Usuário
+			<hr>
+		</h1>
 
 
-				aaaaaaaaaaaaaaaaaaaaaaaaa
+		<div class="grpInputs">
+			<label for="">Nome: </label>
+			<input type="text" name="nome">
+		</div>
 
+		<div class="grpInputs">
+			<label for=""> CPF: </label>
+			<input type="text" name="cpf">
+		</div>
 
+		<div class="grpInputs">
+			<label for="rg"> RG: </label>
+			<input type="number" name="rg"><br>
+			<label for="rgdigt" > Digito: </label>
+			<input type="number" name="rgdigit" style="width:40px;" >
+		</div>
 
+		<div class="grpInputs">
+			<label for="ddd1"> DDD Tel 1: </label>
+			<input type="number" name="ddd1" style="width:40px;"><br>
 
+			<label for="tel"> Tel: </label>
+			<input type="number" name="tel">
+		</div>
 
-			</article>
-		</form>
+		<div class="grpInputs">
+			<label for="ddd2"> DDD Cel 1: </label>
+			<input type="number" name="ddd2" style="width:40px;"><br>
 
+			<label for="cel1"> Cel 1: </label>
+			<input type="number" name="cel1">
+		</div>
 
+		<div class="grpInputs">
+			<label for="ddd3"> DDD Cel 2: </label>
+			<input type="number" name="ddd3" style="width:40px;"><br>
 
-	</div>
-	<?php 
-	// require_once('../_php/conn.php'); 
+			<label for="cel2"> Cel 2: </label>
+			<input type="number" name="cel2">
+		</div>
+
+		<div class="grpInputs">
+			<label for="cep"> CEP: </label>
+			<input type="number" name="cep">
+		</div>
+
+		<div class="grpInputs">
+			<label for="rua"> Rua: </label>
+			<input type="text" name="rua">
+		</div>
+		<div class="grpInputs">
+			<input type="hidden" name="cliente" value="cliente" >
+		</div>
+
+	</form>
+</article>
+
+<?php 
+// 	require_once('CheckAccess.php'); 
+	
 	require_once('footer.php')
-	?>
+?>
